00ce2fe2f4dd61d0f9aa5f61452f3d0c3680581e
vaishnavi6788 <vaishnavitiwari7806@gmail.com>
2026-06-18 22:34:00 +0530

    Create Lengthoflastworld.cpp
    Return the length of the last word in the string

---
+++ Lengthoflastworld.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+        int i = s.length() - 1;
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s[i] != ' ') {
+            len++;
+            i--;
+        }
+
+        return len;
+    }
+};
